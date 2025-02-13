721806a202618863b4b50d9d1f7b951bdf89a67b
emnmtv <90402959+emnmtv@users.noreply.github.com>
2025-02-13 16:02:44 +0800

    1
    1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 8080, // Change this to your desired port number
+  },
   
 })
